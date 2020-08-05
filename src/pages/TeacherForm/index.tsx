c16ea5dae6cb3309db9aa6eb4e68095f37683f7a
unknown <gs_arruda@outlook.com>
2020-08-05 11:22:48 -0300

    initial commit

---
+++ src/pages/TeacherForm/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import {Route, BrowserRouter} from 'react-router-dom'
+
+function TeacherForm(){
+    return(
+        <h1>Form</h1>
+    )
+}
+
+export default TeacherForm;
---
+++ src/pages/TeacherList/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import {Route, BrowserRouter} from 'react-router-dom'
+
+function TeacherList(){
+    return(
+        <h1>Teacher List</h1>
+    )
+}
+
+export default TeacherList
